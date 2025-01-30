54331323962b5d551e8e28ce68443a8be463f2f4
Shanthish23 <shanthish.sr2023cse@sece.ac.in>
2025-01-30 15:47:29 +0530

    Update newRequest.js

--- frontend/src/utils/newRequest.js
+++ frontend/src/utils/newRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const newRequest = axios.create({
-    baseURL:"https://myfiverr-4.onrender.com/api/",
+    baseURL:"https://myfiverr-6.onrender.com/api/",
     withCredentials:true,
 })
 
