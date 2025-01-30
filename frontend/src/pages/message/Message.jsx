e5268d2390d3c2ff69d1edfaef8de1e16658318a
Shanthish23 <shanthish.sr2023cse@sece.ac.in>
2025-01-30 15:13:13 +0530

    Add files via upload

---
+++ frontend/src/pages/message/Message.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Message.scss";
+
+const Message = () => {
+    return (
+        <div className="message">
+        <h1>Message</h1>
+        <div className="img">
+        <img src="/img/underconstruction.jpg" alt="Under Construction" />
+        </div>
+        <h1>Under Construction....</h1>
+        </div>
+    );
+    }
+
+    export default Message;
---
+++ frontend/src/pages/samplepay/samplepay.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./samplepay.scss"
+
+const Samplepay=()=>{
+    return(
+        <div className="pay">
+            <h1>Under Construction</h1>
+            <img src="/img/underconstruction.jpg" alt="Under Construction" />
+        </div>
+        );
+}
+
+export default Samplepay;
